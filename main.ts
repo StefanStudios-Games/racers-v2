1d0efa7e7044fa565a58573f3d6704d295c427e6
stefanspt <63734169+stefanspt@users.noreply.github.com>
2020-06-29 10:06:26 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -37,7 +37,49 @@ c c c c c c c d c c c c c c c c
 c c c c c c c d c c c c c c c c 
 c c c c c c c c c c c c c c c c 
 `
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c e e e e e e e c c c 
+c c c c c e e e e e e e c c c c 
+c c c c e e e e e e e c c c c c 
+c c c e e e e e e e e e c c c c 
+c c e e e e e e e e e e e c c c 
+c e e e e e e e e e e e e e c c 
+c e e e e e e e e e e e e e c c 
+c c e e e e e e e e e e e e e e 
+c c c e e e e e e e e e e c c c 
+c c c c e e e e e e e e e c c c 
+c e e e e e c c c c c e e e c c 
+e e e e e c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+`
 }
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (MyCar.x > 46) {
+        MyCar.x += -16
+    }
+})
+scene.onHitWall(SpriteKind.Player, function (sprite) {
+    if (sprite.isHittingTile(CollisionDirection.Top)) {
+        game.over(true)
+    }
+})
+scene.onHitWall(SpriteKind.Enemy, function (sprite) {
+    if (sprite.isHittingTile(CollisionDirection.Top)) {
+        sprite.destroy()
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (MyCar.x < 110) {
+        MyCar.x += 16
+    }
+})
 function generateCar () {
     randomBand = Math.randomRange(0, 4)
     car1 = sprites.create(img`
@@ -57,13 +99,14 @@ function generateCar () {
 . . . f 8 6 d 8 8 8 8 d 6 8 f . 
 . . . f f 8 8 8 8 8 8 8 8 f f . 
 . . . . f f . . . . . . f f . . 
-`, SpriteKind.Player)
-    car1.setPosition(46 + 16 * randomBand, 94)
+`, SpriteKind.Enemy)
+    car1.setPosition(46 + 16 * randomBand, 650)
     car1.setVelocity(0, -1 * (50 + 20 * randomBand))
 }
-let randomBand = 0
 let car1: Sprite = null
-car1 = sprites.create(img`
+let randomBand = 0
+let MyCar: Sprite = null
+MyCar = sprites.create(img`
 . . . . . . e e c c e e . . . . 
 . . . . . e 2 2 2 2 2 2 e . . . 
 . . . . 2 c 2 2 2 2 2 2 c 2 . . 
@@ -82,7 +125,7 @@ car1 = sprites.create(img`
 . . . . f f . . . . . . f f . . 
 `, SpriteKind.Player)
 tiles.setTilemap(tiles.createTilemap(
-            hex`0a00280003010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            hex`0a00280003010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203030104040404040402030301040404040404020303010404040404040203`,
             img`
 . . . . . . . . . . 
 . . . . . . . . . . 
@@ -125,9 +168,13 @@ tiles.setTilemap(tiles.createTilemap(
 . . . . . . . . . . 
 . . . . . . . . . . 
 `,
-            [myTiles.tile0,sprites.castle.tilePath6,sprites.castle.tilePath4,sprites.castle.tilePath5,myTiles.tile1],
+            [myTiles.tile0,sprites.castle.tilePath6,sprites.castle.tilePath4,sprites.castle.tilePath5,myTiles.tile1,sprites.castle.tileGrass1,myTiles.tile2],
             TileScale.Sixteen
         ))
+scene.cameraFollowSprite(MyCar)
+tiles.placeOnTile(MyCar, tiles.getTileLocation(5, 38))
+MyCar.setPosition(46 + 16 * Math.randomRange(0, 4), 650)
+MyCar.vy = -85
 game.onUpdateInterval(500, function () {
     generateCar()
 })
